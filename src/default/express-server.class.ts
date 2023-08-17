53031c311ae4722dffabef9a310f3670574bc0e6
cjhw <1784581164@qq.com>
2023-08-17 15:57:30 +0800

    feat: router decorator

--- src/default/express-server.class.ts
+++ src/default/express-server.class.ts
@@ -1,22 +1,24 @@
 import * as express from "express";
 import ServerFactory from "../factory/server-factory.class";
 import { bean, log } from "../speed";
+import { setRouter } from "../route-mapping.decorator";
 
 export default class ExpressServer extends ServerFactory {
-    @bean
-    public getSever(): ServerFactory {
-        return new ExpressServer();
-    }
-    public setMiddleware(middleware: any) {
-        this.middlewareList.push(middleware);
-    }
-    public start(port: number) {
-        const app: express.Application = express();
-        this.middlewareList.forEach(middleware => {
-            app.use(middleware);
-        });
-        app.listen(port, () => {
-            log("server start at port: " + port);
-        });
-    }
+  @bean
+  public getSever(): ServerFactory {
+    return new ExpressServer();
+  }
+  public setMiddleware(middleware: any) {
+    this.middlewareList.push(middleware);
+  }
+  public start(port: number) {
+    const app: express.Application = express();
+    this.middlewareList.forEach((middleware) => {
+      app.use(middleware);
+    });
+    setRouter(app);
+    app.listen(port, () => {
+      log("server start at port: " + port);
+    });
+  }
 }
