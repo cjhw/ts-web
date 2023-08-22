23f038fa9ea78a4fa403e3868cf637bff1c8ed7c
cjhw <1784581164@qq.com>
2023-08-22 17:12:09 +0800

    feat: add some middleware

--- src/default/express-server.class.ts
+++ src/default/express-server.class.ts
@@ -1,5 +1,9 @@
 import * as express from "express";
 import * as consolidate from "consolidate";
+import * as serveFavicon from "serve-favicon";
+import * as compression from "compression";
+import * as cookieParser from "cookie-parser";
+import * as expressSession from "express-session";
 import ServerFactory from "../factory/server-factory.class";
 import { bean, log, value } from "../speed";
 import { setRouter } from "../route-mapping.decorator";
@@ -7,6 +11,21 @@ import { setRouter } from "../route-mapping.decorator";
 export default class ExpressServer extends ServerFactory {
   @value("view")
   public view: string;
+
+  @value("static")
+  private static: string;
+
+  @value("favicon")
+  private favicon: string;
+
+  @value("compression")
+  private compression: object;
+
+  @value("cookie")
+  private cookieConfig: object;
+
+  @value("session")
+  private session: object;
 
   @bean
   public getSever(): ServerFactory {
@@ -28,10 +47,41 @@ export default class ExpressServer extends ServerFactory {
   }
 
   private setDefaultMiddleware() {
-    const viewConfig = this.view;
-    this.app.engine(viewConfig["suffix"], consolidate[viewConfig["engine"]]);
-    this.app.set("view engine", viewConfig["suffix"]);
-    this.app.set("views", process.cwd() + viewConfig["path"]);
+    if (this.view) {
+      const viewConfig = this.view;
+      this.app.engine(viewConfig["suffix"], consolidate[viewConfig["engine"]]);
+      this.app.set("view engine", viewConfig["suffix"]);
+      this.app.set("views", process.cwd() + viewConfig["path"]);
+    }
+
+    if (this.session) {
+      const sessionConfig = this.session;
+      if (sessionConfig["trust proxy"] === 1) {
+        this.app.set("trust proxy", 1);
+      }
+      this.app.use(expressSession(sessionConfig));
+    }
+
+    if (this.static) {
+      const staticPath = process.cwd() + this.static;
+      this.app.use(express.static(staticPath));
+    }
+
+    if (this.favicon) {
+      const faviconPath = process.cwd() + this.favicon;
+      this.app.use(serveFavicon(faviconPath));
+    }
+
+    if (this.compression) {
+      this.app.use(compression(this.compression));
+    }
+
+    this.app.use(
+      cookieParser(
+        this.cookieConfig["secret"] || undefined,
+        this.cookieConfig["options"] || {}
+      )
+    );
 
     setRouter(this.app);
   }
